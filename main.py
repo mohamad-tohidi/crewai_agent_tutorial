a581e23b4d71a939e9499532b8b4700699a6270d
Tohidi <the.mohammad.tohidi@gmail.com>
2025-08-20 14:22:53 +0330

    feat: implementing the actual example

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from crewai.flow.flow import Flow, listen, start
+from dotenv import load_dotenv
+from litellm import completion
+
+load_dotenv()
 
 
 class ExampleFlow(Flow):
+    model = "openai/gemma-3"
+
     @start()
     def generate_city(self):
-        return "Qom"
+        print("Starting the flow")
+
+        response = completion(
+            model=self.model,
+            messages=[
+                {"role": "user", "content": "یک شهر رندوم از کشور ایران رو برگردون"}
+            ],
+        )
+        random_city = response.choices[0].message.content
+
+        print(f"Random City:{random_city}")
+        return random_city
 
     @listen(generate_city)
     def generate_fun_fact(self, random_city):
-        pass
+        response = completion(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"یک واقعیت جالب درباره این شهر بهم بگو {random_city}",
+                }
+            ],
+        )
+
+        fun_fact = response.choices[0].message.content
+        return fun_fact
 
 
 flow = ExampleFlow()
 result = flow.kickoff()
 
-print(f"generated fun fact for {result}")
+print(result)
